2801264e2b6d5b23a5e80a953af78d8cb6a4d208
Shay Keinan <crazypixel@gmail.com>
2019-11-28 11:33:30 +0200

    setuop - with app-comps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {ThemeProvider} from 'styled-components';
+import {AppTheme, lightTheme} from '@datorama/app-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+	return (
+		<AppTheme theme={lightTheme} provider={ThemeProvider}>
+			<Container>
+			
+			</Container>
+		</AppTheme>
+	);
+};
 
 export default App;
+
+const Container = styled.div`
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
